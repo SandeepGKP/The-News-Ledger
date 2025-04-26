eb001d9bebd9531db9d57d8ee33febd115e0c9ed
SandeepGKP <125468429+SandeepGKP@users.noreply.github.com>
2025-04-26 19:56:08 +0530

    Update login.js

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -10,7 +10,7 @@ const Login = ({ onLogin }) => {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/login', { username, password });
+      const res = await axios.post('https://the-news-ledger-1.onrender.com/api/login', { username, password });
       localStorage.setItem('isLoggedIn', 'true');
       localStorage.setItem('token', res.data.token);
       onLogin();
